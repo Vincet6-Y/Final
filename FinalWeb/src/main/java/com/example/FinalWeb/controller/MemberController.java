d29f339ff408237d5adccd19f64acd5c5269d5f1
Vincent <vincenty5106@gmail.com>
2026-03-05 16:25:06 +0800

    FileTesting

---
+++ FinalWeb/src/main/java/com/example/FinalWeb/controller/MemberController.java
@@ -0,0 +1,18 @@
+package com.example.FinalWeb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/")
+@Controller
+public class MemberController {
+    @RequestMapping("/test")
+    public String test() {
+        return "homeTest";
+    }
+
+    @RequestMapping("/index")
+    public String index() {
+        return "reactTest";
+    }
+}
