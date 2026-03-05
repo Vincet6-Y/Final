bb3b64835e7ee7aae4ef4046b4c0399e67ecaf8d
Vincent <vincenty5106@gmail.com>
2026-03-05 09:43:06 +0800

    0305_建立 JAVA init

---
+++ FinalWeb/src/main/java/com/example/FinalWeb/FinalWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.FinalWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalWebApplication.class, args);
+	}
+
+}
