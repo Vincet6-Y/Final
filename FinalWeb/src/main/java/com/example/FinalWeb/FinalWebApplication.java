8007642b7542ce3dab9abc2d446044a44f2f68dd
leo77vl <leobee0817@gmail.com>
2026-03-09 23:51:11 +0800

    有開啟資料庫顯示，並確認是否支付成功

--- FinalWeb/src/main/java/com/example/FinalWeb/FinalWebApplication.java
+++ FinalWeb/src/main/java/com/example/FinalWeb/FinalWebApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // 輸入即可以不用開啟資料庫並確認網頁版面
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+// import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-// @SpringBootApplication
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
+// @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class FinalWebApplication {
 
 	public static void main(String[] args) {
