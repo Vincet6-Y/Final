f3c82210957ebdb9b1a4d01621a9e14c939c7fa2
Tony191911 <fentony5@gmail.com>
2026-03-11 11:42:33 +0800

    深淺色切換文字顏色修正，卡片高度及動畫修正中(未完成)

--- FinalWeb/src/main/java/com/example/FinalWeb/controller/MemberController.java
+++ FinalWeb/src/main/java/com/example/FinalWeb/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.example.FinalWeb.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.FinalWeb.repo.OrdersRepo;
