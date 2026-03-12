04bda5d21a2259f2ecf283f7e9076ad9db64b292
yta <yta19960425@gmail.com>
2026-03-12 20:52:14 +0800

    首頁搜尋欄修改

--- FinalWeb/src/main/java/com/example/FinalWeb/controller/WorkController.java
+++ FinalWeb/src/main/java/com/example/FinalWeb/controller/WorkController.java
@@ -1,7 +1,6 @@
 package com.example.FinalWeb.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
@@ -9,48 +8,44 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.example.FinalWeb.entity.WorkDetailEntity;
 import com.example.FinalWeb.service.WorkService;
 
 @Controller
-@RequestMapping("/")
 public class WorkController {
 
     @Autowired
     private WorkService service;
 
-    // 測試抓 TiDB 的資料用 RestController，需要強制轉 JSON
-    // @GetMapping(value = "/test", produces = "application/json")
-    // public List<WorkDetailEntity> getWord() {
-    // return service.getWork();
-    // }
+    // 首頁邏輯
+    @GetMapping("/home")
+    public String home(Model model) {
+        System.out.println("=== 偵測到訪問首頁 ===");
+        
+        // 抓取資料
+        List<WorkDetailEntity> featuredList = service.getWorkList(0, 4, "DESC", null).getContent();
+        
+        model.addAttribute("featuredList", featuredList);
+        return "home";
+    }
 
+    // 列表頁邏輯
     @GetMapping("/workList")
-    public String getWorkPage(Model model, @RequestParam(defaultValue = "0") int page,
+    public String getWorkPage(Model model, 
+            @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "4") int size,
             @RequestParam(defaultValue = "DESC") String sortDir,
             @RequestParam(required = false) String workClass) {
         Page<WorkDetailEntity> workPage = service.getWorkList(page, size, sortDir, workClass);
-
         model.addAttribute("works", workPage);
-        model.addAttribute("page", page);
-        model.addAttribute("sortNow", sortDir);
-        model.addAttribute("classNow", workClass);
-
         return "workList";
     }
 
-    @RequestMapping("/workListDetail")
-    public String workListDetail(Model model,
-            @RequestParam(value = "workId") int workId) {
-
+    // 詳情頁邏輯
+    @GetMapping("/workListDetail")
+    public String workListDetail(Model model, @RequestParam int workId) {
         WorkDetailEntity gEntity = service.getWorkId(workId);
-
         model.addAttribute("idNow", gEntity);
-
         return "workListDetail";
     }
-
 }
