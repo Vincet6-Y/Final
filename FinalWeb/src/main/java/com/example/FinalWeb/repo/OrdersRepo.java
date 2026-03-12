804a914a4c7268889f9da7bbe3582a605ffd5ecd
leo77vl <leobee0817@gmail.com>
2026-03-12 12:09:25 +0800

    變更訂單明細邏輯，新增票券選項

--- FinalWeb/src/main/java/com/example/FinalWeb/repo/OrdersRepo.java
+++ FinalWeb/src/main/java/com/example/FinalWeb/repo/OrdersRepo.java
@@ -1,5 +1,7 @@
 package com.example.FinalWeb.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,11 @@ import com.example.FinalWeb.entity.OrdersEntity;
 
 @Repository
 public interface OrdersRepo extends JpaRepository<OrdersEntity, Integer> {
+    // 1. 給綠界金流用的：用交易編號找訂單
+    // 思考邏輯：綠界付款成功後，會發送通知給你的伺服器並附帶 TradeNo。
+    OrdersEntity findByTradeNo(String tradeNo);
+
+    // 2. 給會員系統用的：找某個會員的所有訂單
+    // 思考邏輯：會員登入後，要去「我的訂單」頁面查看買過的景點票券。
+    List<OrdersEntity> findByMember_MemberId(Integer memberId);
 }
