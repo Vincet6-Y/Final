957ac3137cc5a25f01c338915320b2ebb43bc04d
Vincent <vincenty5106@gmail.com>
2026-03-19 16:19:43 +0800

    後台作品列表管理部份尚缺：列出全作品、編輯功能(考慮)

--- FinalWeb/src/main/java/com/example/FinalWeb/controller/BackEndController.java
+++ FinalWeb/src/main/java/com/example/FinalWeb/controller/BackEndController.java
@@ -1,11 +1,18 @@
 package com.example.FinalWeb.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.FinalWeb.dto.WorkDTO;
+import com.example.FinalWeb.entity.WorkDetailEntity;
 import com.example.FinalWeb.service.WorkService;
 
 @Controller
@@ -31,7 +38,11 @@ public class BackEndController {
     }
 
     @RequestMapping("/contentmanagement")
-    public String backendcontentmanagement() {
+    public String backendcontentmanagement(Model model) {
+        List<WorkDetailEntity> getNew5 = service.showNew5();
+
+        model.addAttribute("newWroks", getNew5);
+
         return "/backend/backendcontentmanagement";
     }
 
@@ -40,13 +51,15 @@ public class BackEndController {
         return "/backend/backendarticle";
     }
 
+    // 作品列表相關
     @RequestMapping("/contentmanagement/work")
     public String backendwork() {
+
         return "/backend/backendworkmanagement";
     }
 
     @PostMapping("/contentmanagement/work/add")
-    public String workupload(WorkDTO wDto){
+    public String workupload(WorkDTO wDto) {
         service.addwork(wDto);
 
         return "redirect:/backend/contentmanagement/work";
